7fe5ff35b3f2b08f69368f39f5fc17c092a9ae50
Joris Deguet <joris.deguet@gmail.com>
2024-03-16 08:17:11 -0400

    Doc

--- immo/centris.py
+++ immo/centris.py
@@ -2,6 +2,17 @@
 from shared import click_by_id, setup,  select_parc_ex, select_type, \
     startSearch, selectPrice, explore_and_send
 
+# pour ubuntu, en headless, certains trucs ne marchent pas
+# il fallait le driver avec la bonne version de chrome
+# uniquement trouvé le driver pour la version  ici
+# https://chrome-versions.com/google-chrome-stable-114.0.5735.90-1.deb
+# https://chromedriver.storage.googleapis.com/index.html?path=114.0.5735.90/
+
+# si on met à jour Chrome, le script ne marche plus
+
+# le script va sur la page, met les critères re recherche, itère sur les pages
+# collecte les urls, compare avec les urls connues, envoie un sms si nouvelle url
+
 driver = setup()
 driver.get("https://www.centris.ca/")
 driver.implicitly_wait(5)
